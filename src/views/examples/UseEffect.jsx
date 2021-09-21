bcb35080d3b3521b3294039b87668fc31e9301c5
Junior Lourenço <juniorminaj101@gmail.com>
2021-09-21 15:06:00 -0400

    useEffect

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle';
 
 function calcFatorial (num) {
     const n = parseInt(num)
@@ -9,11 +10,27 @@ function calcFatorial (num) {
 }
 
 const UseEffect = (props) => {
+
+    //Ex #01
+
     const [number, setNumber] = useState(1);
     const [fatorial, setFatorial] = useState(1);
 
     useEffect(function() {
         setFatorial(calcFatorial(number));
+    }, [number])
+
+    useEffect(function() {
+        if(fatorial > 1000000){
+            document.title = "Eita!!!"
+        }
+    }, [fatorial])
+
+    //Ex #02
+    const [status, setStatus] = useState("Ímpar")
+
+    useEffect(function() {
+        setStatus(number % 2 === 0 ? "Par" : "Ímpar")
     }, [number])
 
     return (
@@ -22,14 +39,23 @@ const UseEffect = (props) => {
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
+            <SectionTitle title="Exercício #01" />
             <div className="center">
                 <div>
-                <span className="text">Fatorial:</span>
-                <span className="text red">{fatorial === -1 ? 'Não existe' : fatorial}</span>
-                <input type="number" 
-                    className="input" 
-                    value={number} 
-                    onChange={e=> setNumber(e.target.value)}/>
+                    <span className="text">Fatorial:</span>
+                    <span className="text red">{fatorial === -1 ? 'Não existe' : fatorial}</span>
+                    <input type="number" 
+                        className="input" 
+                        value={number} 
+                        onChange={e=> setNumber(e.target.value)}/>
+                </div>
+
+                <SectionTitle title="Exercício #02" />
+                <div className="center">
+                    <div>
+                        <span className="text">Status:</span>
+                        <span className="text red">{status}</span>
+                    </div>
                 </div>
             </div>
         </div>
