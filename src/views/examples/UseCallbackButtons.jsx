633eb1870e673dcaf0ad25ba9ce9d9e084715381
Junior Lourenço <juniorminaj101@gmail.com>
2021-09-22 14:01:12 -0400

    useCallback

---
+++ src/views/examples/UseCallbackButtons.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const UseCallbackButtons = props => {
+    return(
+        <div>
+            <button 
+                    className="btn"
+                    onClick={ ()=> props.inc(6)}
+                >
+                    +6
+                </button>
+                <button 
+                    className="btn"
+                    onClick={ ()=> props.inc(12)}
+                >
+                    +12
+                </button>
+                <button 
+                    className="btn"
+                    onClick={ ()=> props.inc(18)}    
+                >
+                    +18
+                </button>
+        </div>
+    )
+}
+
+export default UseCallbackButtons;
